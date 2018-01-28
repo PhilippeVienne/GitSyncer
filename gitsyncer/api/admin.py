3aa64cefeb8eab39118c7a3d4a71674d5b09e0f5
Philippe Vienne <philippegeek@gmail.com>
2018-01-28 17:23:18 +0100

    Able to sync repositories

--- gitsyncer/api/admin.py
+++ gitsyncer/api/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 
-from gitsyncer.api.models import Mirror
+from gitsyncer.api.models import Mirror, Repository, SSHKey
 
 
 @admin.register(Mirror)
 class MirrorAdmin(admin.ModelAdmin):
-    pass
+
+    list_display = ('id', 'origin', 'destination', 'owner')
+    search_fields = ('origin__name', 'destination__name')
+
+
+@admin.register(Repository)
+class RepositoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'remote_url')
+    search_fields = ('name', 'remote_url')
+
+
+@admin.register(SSHKey)
+class SSHKeyAdmin(admin.ModelAdmin):
+
+    list_display = ('__str__', 'public_key')
+    search_fields = ('id',)
+
+    def get_changeform_initial_data(self, request):
+        return SSHKey.gen_keys()
--- gitsyncer/api/models.py
+++ gitsyncer/api/models.py
@@ -1,21 +1,94 @@
 import os
+import stat
+
+from cryptography.hazmat.primitives import serialization as crypto_serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
 from django.conf import settings
+from django.contrib.auth.models import Group
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from git import Repo
+from git import Repo, Git
+
+
+class SSHKey(models.Model):
+    private_key = models.TextField(blank=True)
+    public_key = models.TextField(blank=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.private_key is None or self.private_key == '':
+            key = self.gen_keys()
+            self.private_key = key['private_key']
+            self.public_key = key['public_key']
+            pass
+
+    @staticmethod
+    def gen_keys():
+        key = rsa.generate_private_key(
+            backend=crypto_default_backend(),
+            public_exponent=65537,
+            key_size=2048
+        )
+        return {
+            'private_key':
+                (key.private_bytes(
+                    crypto_serialization.Encoding.PEM,
+                    crypto_serialization.PrivateFormat.PKCS8,
+                    crypto_serialization.NoEncryption())).decode('utf-8'),
+            'public_key':
+                (key.public_key().public_bytes(
+                    crypto_serialization.Encoding.OpenSSH,
+                    crypto_serialization.PublicFormat.OpenSSH
+                )).decode('utf-8')
+        }
+
+    def __str__(self):
+        return "SSH-{}".format(self.pk)
+
+    def get_file(self):
+        ssh_file = self._get_path()
+        with open(ssh_file, "w") as text_file:
+            text_file.write(self.private_key)
+        os.chmod(ssh_file, stat.S_IRWXU)
+        return ssh_file
+
+    def _get_path(self):
+        return os.path.join(settings.CLONE_ROOT, ".id_ssh_%s" % self.pk)
+
+
+class Repository(models.Model):
+    name = models.CharField(max_length=250, verbose_name=_('nom'))
+    remote_url = models.CharField(max_length=2500, verbose_name=_('url du dépot'))
+    ssh_key = models.ForeignKey(SSHKey, on_delete=models.PROTECT, null=True, blank=True)
+
+    def get_ssh_command(self):
+        if self.ssh_key_id is None:
+            return 'ssh'
+        else:
+            return 'ssh -o IdentitiesOnly=yes -i %s' % self.ssh_key.get_file()
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.remote_url)
 
 
 class Mirror(models.Model):
-    origin = models.CharField(max_length=250, verbose_name=_('origine'))
-    destination = models.CharField(max_length=250, verbose_name=_('destination'))
+    origin = models.ForeignKey(Repository, verbose_name=_('origine'), on_delete=models.CASCADE,
+                               related_name='mirrors_as_origin')
+    destination = models.ForeignKey(Repository, verbose_name=_('destination'), on_delete=models.CASCADE,
+                                    related_name='mirrors_as_destination')
     enabled = models.BooleanField(verbose_name=_('activé'), default=True)
+    owner = models.ForeignKey(Group, verbose_name=_('propriétaire'), on_delete=models.PROTECT)
     last_sync = models.DateTimeField(verbose_name=_('dernière synchronisation'), null=True, blank=True)
 
     def sync(self):
+        os.makedirs(settings.CLONE_ROOT, exist_ok=True)
         path = os.path.join(settings.CLONE_ROOT, str(self.pk))
+        origin_env = dict(GIT_SSH_COMMAND=self.origin.get_ssh_command())
+        destination_env = dict(GIT_SSH_COMMAND=self.destination.get_ssh_command())
         if not os.path.exists(path):
-            repo = Repo.clone_from(self.origin, path, mirror=True)
+            repo = Repo.clone_from(self.origin.remote_url, path, mirror=True, env=origin_env)
             origin = repo.remote('origin')
         else:
             repo = Repo(path)
@@ -23,6 +96,6 @@ class Mirror(models.Model):
         try:
             dest_remote = repo.remote('destination')
         except ValueError:
-            dest_remote = repo.create_remote('destination', self.destination)
-        origin.update()
-        dest_remote.push(mirror=True)
+            dest_remote = repo.create_remote('destination', self.destination.remote_url, env=destination_env)
+        origin.update(env=origin_env)
+        dest_remote.push(mirror=True, env=destination_env)
